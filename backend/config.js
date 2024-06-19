7aa2cb286d87c693122a8947d92097d7c749a5f5
Hardik Vyas <hardik.vyas@kenscio.com>
2024-06-19 15:30:06 +0530

    Adding frontend and backend

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5655;
+
+export const mongoDBURL =
+  'mongodb+srv://hardikvyasdev:6971vyas@books-store-mern.irp7cf8.mongodb.net/?retryWrites=true&w=majority&appName=Books-Store-MERN';
